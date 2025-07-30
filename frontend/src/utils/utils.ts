a86551aa531831d4490337418fdd4ecbda8ac804
Najmeh Miri <najme.miri@gmail.com>
2025-07-30 22:34:58 +0200

    add utility fuction

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export type RowColType = {
+  row: number | null;
+  col: number | null;
+};
+
+export function groupEventTypesByPostfix(eventTypes: string[]) {
+  const groups: Record<string, string[]> = {};
+  eventTypes.forEach((eventType) => {
+    const parts = eventType.split("_");
+    const postfix = parts.length > 1 ? parts[parts.length - 1] : eventType;
+    if (!groups[postfix]) groups[postfix] = [];
+    groups[postfix].push(eventType);
+  });
+  return Object.values(groups).flat();
+};
